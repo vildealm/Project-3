b998ce6c5a13c96941b3109c8c1cc2cf49b9baa3
birgitbs <birgitbs@stud.ntnu.no>
2020-10-12 15:39:44 +0200

    created folders for redux

--- src/Reducers/reducer.ts
+++ src/Reducers/reducer.ts
@@ -1,16 +1,12 @@
 //A function that takes state and action as arguments, and returns the next state of the app. Inspired by: https://medium.com/better-programming/redux-setup-for-your-react-app-d003ec03aedf
 
-import { VisibilityFilters } from "../actions/Action";
 
 
  const defaultState = {
     currentPerson: {}
 }
 
-const initialState = {
-    visibilityFilter: VisibilityFilters.SHOW_ALL,
-    todos: []
-  }
+
 
 
 function Reducer( state = defaultState, action: any){
