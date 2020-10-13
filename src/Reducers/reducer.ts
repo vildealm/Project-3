e72d9497c552e2294841e18b4163a889d9fa70b9
birgitbs <birgitbs@stud.ntnu.no>
2020-10-13 10:32:28 +0200

    m

--- src/Reducers/reducer.ts
+++ src/Reducers/reducer.ts
@@ -1,12 +1,9 @@
 //A function that takes state and action as arguments, and returns the next state of the app. Inspired by: https://medium.com/better-programming/redux-setup-for-your-react-app-d003ec03aedf
 
 
-
  const defaultState = {
     currentPerson: {}
 }
-
-
 
 
 function Reducer( state = defaultState, action: any){
