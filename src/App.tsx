9257b13175d6aeba410ad4ed23b188640d4ec79e
birgitbs <birgitbs@stud.ntnu.no>
2020-10-14 10:44:50 +0200

    intro to frontend + styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import { List } from './Components/List/List';
-import { Filter } from './Components/List/Filter/Filter';
+import { Dropdown } from './Dropdown/Dropdown';
+import { Output } from './Components/List/Output/Output';
+import { Search } from './Components/List/Search';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> List of people </h1>
-        <List/>
-        <Filter/>
+        <h1> Visage Lexicon </h1>
+        <Search/>
+        <Dropdown/>
+        <Output/>
+        
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import {createStore} from 'redux';
-import Reducer from './reducers/Reducer';
 
-const store = createStore(Reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store = {store}>
+  <React.StrictMode>  
     <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
