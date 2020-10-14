88f2449e99fc4cfca74f90f48ed2d339cab783fd
Vilde Almestad <vildealmestad@MacBook-Air-3.local>
2020-10-14 14:35:18 +0200

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import './App.css';
 import { Dropdown } from './Dropdown/Dropdown';
 import { Output } from './Components/List/Output/Output';
 import { Search } from './Components/List/Search';
-import {Provider} from 'react-redux'; 
-import {store} from './store'; 
+
 
 function App() {
   return (
