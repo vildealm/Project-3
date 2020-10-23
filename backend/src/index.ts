2505f0c150ec11e57b234c46cfd5300accec0f30
birgitbs <birgitbs@stud.ntnu.no>
2020-10-23 16:08:56 +0200

    cleaning up

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,15 +16,4 @@ export const startServer = async () => {
 }
 
 
-/*const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
-const server = new ApolloServer({ typeDefs, resolvers });
-const app = express();
-server.applyMiddleware({ app });
-
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);*/
-
 startServer();
