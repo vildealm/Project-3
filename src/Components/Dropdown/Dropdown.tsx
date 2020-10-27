81ba09aec97217430af71f19b5cc5113fdf99591
birgitbs <birgitbs@stud.ntnu.no>
2020-10-27 12:26:31 +0100

    frontend functionality for chosing age and location filter

--- src/Components/Dropdown/Dropdown.tsx
+++ src/Components/Dropdown/Dropdown.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import './Dropdown.css';
 
+//dette er den valgte alderen det skal filtreres på
+let ageFilter; 
+let locationFilter; 
+
 function submitAgeFilter(){
+    let inputElement: HTMLInputElement = document.getElementById('number') as HTMLInputElement;
+    let value: string = inputElement.value;
+    ageFilter = parseInt(value);
+}
 
-
-    //add age filter
-
+function submitLocationFilter(location : string ) : any {
+    locationFilter = location;
+    
 }
 
 export function Dropdown() : any {
-
- 
-
-
     return (
         <div>      
-
             <div className = "dropdown-age">
                 <label className = "age-label" > Age: </label>
-                <input value="21" className = "age" type="number" id="number" min="20" max="100" ></input>
+                <input className = "age" type="number" id="number" min="1" max="100"  ></input>
                 <button className = "age-button" onClick={submitAgeFilter}> &rarr;</button>
                 
             </div>      
@@ -27,9 +30,10 @@ export function Dropdown() : any {
             <div className="dropdown-location">
                 <button className="dropbtn">Location ▼</button>
                 <div className="dropdownContent">
-                    <a>Trondheim</a>
-                    <a>Torino</a>
-                    <a>Oslo</a>
+                    <a onClick={submitLocationFilter("Trondheim")}>Trondheim</a>
+                    <a onClick={submitLocationFilter("Torino")}>Torino</a>
+                    <a onClick={submitLocationFilter("Oslo")}>Oslo</a>
+                    <a onClick={submitLocationFilter("Washington")}>Washington</a>
                 </div>
             </div>
             <div className="dropdown-sorting">
@@ -37,7 +41,6 @@ export function Dropdown() : any {
                 <div className="dropdownContent">
                     <a>Alphabetical</a>
                     <a>Age</a>
-                    <a>Time added</a>
                 </div>
             </div>
         </div> 
