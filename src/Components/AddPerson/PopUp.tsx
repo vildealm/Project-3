690a9d410b3206d1fc857e0d2ed8906dc72be267
Vilde Almestad <vildealmestad@dhcp-10-220-215-111.wlan.ntnu.no>
2020-10-27 14:35:09 +0100

    joih

--- src/Components/AddPerson/PopUp.tsx
+++ src/Components/AddPerson/PopUp.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import './PopUp.css';
-import { gql, useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import { useMutation } from 'react-apollo';
 
 const ADD_PERSON = gql`
   mutation register($firstname: String!, $lastname: String!, $age: Int!, $location: String!, $description: String!) {
-    register(firstname: $firstname, lastname: $lastname, age: $age, location: $location, description: $description) {
+    register(input:{firstname: $firstname, lastname: $lastname, age: $age, location: $location, description: $description}) {
       user_id
       first_name
       last_name
@@ -41,45 +42,36 @@ export const PopUp = () => {
     const [location, setLoc] = useState('');
     const [description, setDesc] = useState('');
 
-
-    //const [addPerson, { data }] = useMutation<PersonInventory, NewPersonDetails>(ADD_PERSON);
-
-
-    const [addPerson, { error, data }] = useMutation<PersonInventory, NewPersonDetails>
+    const [addPerson, { error, data }] = useMutation<{addPerson:PersonInventory}, NewPersonDetails>
     (ADD_PERSON, {variables: {first_name, last_name, age: +age, location, description }});
 
     return (
         <div>
-            <h3>Add a Person</h3>
             {error ? <p>Oh no! {error.message}</p> : null}
-            {data && 
-            data.first_name &&
-            data.last_name &&
-            data.age &&
-            data.location && 
-            data.description ? <p>Saved!</p> : null}
+            {data && data.addPerson ? <p>Saved!</p> : null}
             <form className="form-popup">
                 <label>
-                    Firstname: <input type="text" name="fname" onChange={e => setFirstName(e.target.value)} required />
+                    Firstname: <input type="text" name="first_name" onChange={(e) => setFirstName(e.target.value)}/>
                 </label>
                 <label>
-                    Lastname: <input type="text" name="lname" onChange={e => setLastName(e.target.value)} required />
+                    Lastname: <input type="text" name="last_name" onChange={(e) => setLastName(e.target.value)}/>
                 </label>
                 <label>
-                    Age: <input type="number" name="age" onChange={e => setAge(+e.target.value)} required />
+                    Age: <input type="number" name="age" onChange={(e) => setAge(+e.target.value)} required />
                 </label>
                 <label>
-                    Location: <input type="text" name="location" onChange={e => setLoc(e.target.value)} required />
+                    Location: <input type="text" name="location" onChange={(e) => setLoc(e.target.value)} required />
                 </label>
                 <label>
-                    Bio: <input type="text" name="bio" onChange={e => setDesc(e.target.value)} required />
+                    Bio: <input type="text" name="bio" onChange={(e) => setDesc(e.target.value)} required />
                 </label>
                 <button className="submit-form-btn" type="submit" onClick={() =>
                     first_name &&
                     last_name &&
                     age &&
                     location &&
-                    description}> 
+                    description && 
+                    addPerson()}> 
                     Submit
                     </button>
             </form>
