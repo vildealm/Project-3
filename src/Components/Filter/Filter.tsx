2e479963a6ab41cee834e086b6d04fbb4114e28c
birgitbs <birgitbs@stud.ntnu.no>
2020-10-27 16:21:05 +0100

    finished frontent functionality for filter on age and search

--- src/Components/Filter/Filter.tsx
+++ src/Components/Filter/Filter.tsx
@@ -2,42 +2,44 @@ import React from 'react';
 import './Filter.css';
 
 
-
-
-
-
 //dette er den valgte alderen det skal filtreres på
 let ageFilter; 
 let locationFilter : any; 
+let searchFilter : any; 
 
-function submitAgeFilter(){
+function submitAgeFilter(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault();
     let inputElement: HTMLInputElement = document.getElementById('number') as HTMLInputElement;
     let value: string = inputElement.value;
     ageFilter = parseInt(value);
+    console.log(ageFilter)
 }
 
 function submitLocationFilter(location : string ) : any {
     locationFilter = location;
-    
+}
+
+function submitSearchFilter( event: React.FormEvent<HTMLFormElement>) : any {
+    event.preventDefault();
+    let searchElement: HTMLInputElement = document.getElementById("search") as HTMLInputElement;
+    searchFilter = searchElement.value;
 }
 
 export function Filter() : any {
     return (
         <div>  
-  
             <div className="search-container">
-            <input
-            type = "text"
-            className="search"
-            placeholder = "Search ..."
-            >
-            </input>
+                <form onSubmit= {submitSearchFilter}>
+                    <input id="search" type = "text" className="search" placeholder = "Search ..."/>
+                    <input type="submit" className="search-button" value="Go" />
+                </form>
             </div>    
             <div className = "dropdown-age">
-                <label className = "age-label" > Age: </label>
-                <input className = "age" type="number" id="number" min="1" max="100"  ></input>
-                <button className = "age-button" onClick={submitAgeFilter}> &rarr;</button>
-                
+                <form onSubmit={submitAgeFilter}>
+                    <label className = "age-label" > Age: </label>
+                    <input className = "age" type="number" id="number" min="1" max="100"  ></input>
+                    <button type="submit" className = "age-button" > &rarr;</button>
+                </form>
             </div>      
 
             <div className="dropdown-location">
