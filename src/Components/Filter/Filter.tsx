a4820a66a9fd2a7d87ef063f7e6afa3d91139483
Edvard Mattias Bø <edvardmb@stud.ntnu.no>
2020-10-29 12:59:11 +0100

    Filtering kos

--- src/Components/Filter/Filter.tsx
+++ src/Components/Filter/Filter.tsx
@@ -5,13 +5,13 @@ import { QueryResult, useLazyQuery } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Description } from '@material-ui/icons';
 import Person from '../Person/Person';
-import { checkDocument } from '@apollo/client/utilities';
 
 
 function setPerson(queryResult: QueryResult){
 
     let people : any = []; 
     let ids : any = [];
+    let input : String; 
 
     let person = {
         id: Number,
@@ -21,28 +21,62 @@ function setPerson(queryResult: QueryResult){
         location: String,
         description: String
     }
-    let key = 0;
-    let stop : boolean = false;
-    if(queryResult.data !== undefined && !stop){
-        queryResult.data.persons.map(({id, first_name, last_name, age, location, description}: any) => {
-            console.log(first_name, last_name);
-            person.id= id;
-            person.first_name = first_name;
-            person.last_name = last_name;
-            person.age = age;
-            person.location = location;
-            person.description = description;
-            if(!ids.includes(person.id)){
-            people.push(<Person first_name= {person.first_name} last_name = {person.last_name} location = {person.location} age= {person.age} description = {person.description}/>);
-            ids.push(person.id);
-            }
-        });
+    if(queryResult.error){
+        return <p>{queryResult.error}</p>;
+    }
+    
+    if(queryResult.data !== undefined){
+        if(queryResult.data.persons !== undefined){
+            queryResult.data.persons.map(({id, first_name, last_name, age, location, description}: any) => {
+                console.log(first_name, last_name);
+                person.id= id;
+                person.first_name = first_name;
+                person.last_name = last_name;
+                person.age = age;
+                person.location = location;
+                person.description = description;
+                if(!ids.includes(person.id)){
+                people.push(<Person first_name= {person.first_name} last_name = {person.last_name} location = {person.location} age= {person.age} description = {person.description}/>);
+                ids.push(person.id);
+                }
+            });
+        }
+        else if(queryResult.data.filterSearch !== undefined){
+            queryResult.data.filterSearch.map(({id, first_name, last_name, age, location, description}: any) => {
+                console.log(first_name, last_name);
+                person.id= id;
+                person.first_name = first_name;
+                person.last_name = last_name;
+                person.age = age;
+                person.location = location;
+                person.description = description;
+                if(!ids.includes(person.id)){
+                people.push(<Person first_name= {person.first_name} last_name = {person.last_name} location = {person.location} age= {person.age} description = {person.description}/>);
+                ids.push(person.id);
+                }
+            });
+        }
+        else{
+            queryResult.data.nameSearch.map(({id, first_name, last_name, age, location, description}: any) => {
+                console.log(first_name, last_name);
+                person.id= id;
+                person.first_name = first_name;
+                person.last_name = last_name;
+                person.age = age;
+                person.location = location;
+                person.description = description;
+                if(!ids.includes(person.id)){
+                people.push(<Person first_name= {person.first_name} last_name = {person.last_name} location = {person.location} age= {person.age} description = {person.description}/>);
+                ids.push(person.id);
+                }
+            });
+        }
     }
     return people;
 }
 
 const GET_ALL = gql`
-    query getAll( $orderBy: String!){
+    query persons( $orderBy: String!){
         persons(orderBy: $orderBy){
             id
             first_name
@@ -101,7 +135,7 @@ export function Filter() : any {
         }
     }
 
-    const [getAll, allResults] = useLazyQuery(
+    const [persons, allResults] = useLazyQuery(
         GET_ALL,
         { variables: { orderBy: orderBy } }
     );
@@ -114,8 +148,7 @@ export function Filter() : any {
         { variables: { age: age, location: location, orderBy: orderBy} });
     console.log(filterResults.data);
     console.log(nameResults.data);
-    
-    //setPerson(nameResults);
+    console.log(allResults.data);
 
     return (
         <div>  
@@ -166,9 +199,9 @@ export function Filter() : any {
                     <a onClick={()=> {setOrderBy("first_name");setOrderOutput("Alphabetical");}}>Alphabetical</a>
                     <a onClick={()=> {setOrderBy("age");setOrderOutput("Age");}}>Age</a>
                 </div>
-                <div>
-                    {setPerson(checkStatus(activeFilter))}
-                </div>
+            </div>
+            <div>
+                {setPerson(checkStatus(activeFilter))}
             </div>
         </div> 
     );
