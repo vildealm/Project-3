2f737f1d41fe0c65b0b46a84509062199c5296cc
birgitbs <birgitbs@stud.ntnu.no>
2020-10-30 22:09:14 +0100

    test

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
 import Modal from 'react-modal'
 
