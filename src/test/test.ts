e891bd962733fe9e792d9599989fafac6c7d06ba
Vilde Almestad <vildealmestad@dhcp-10-220-215-111.wlan.ntnu.no>
2020-10-30 13:55:45 +0100

    Cypress and Mocha test

---
+++ src/test/test.ts
@@ -0,0 +1,18 @@
+import 'mocha';
+import {Filter} from './Filter/Filter'; 
+
+describe('Page turner', function(){
+  describe('#nextPage()', function() {
+    it('should return the next page', function() {
+      expect(Filter.pageNumber).toEqual(10)
+    });
+  });
+});
+
+describe('Page turner', function() {
+  describe('#previousPage()', function() {
+    it('should return the previous page', function() {
+      expect(Filter.pageNumber).toEqual(0)
+    });
+  });
+});
